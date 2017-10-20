077cc5a55f969779bba7c338907f76697c0aa630
Unknown <cstubbs@ga.tmsschool.ca>
2017-10-20 07:11:03 -0400

    Everything is done except pictures

---
+++ site/js/imageSwap.js
@@ -0,0 +1,16 @@
+//(function($) {
+//  "use strict"; // Start of use strict
+//
+//  $('#team img').hover( function(){
+//     var temp = $(this).attr('src');
+//      temp = temp.slice(0, -4);
+//      if(temp[temp.length -1] == '2'){
+//          temp = temp.slice(0,-1);
+//      }
+//      else{
+//        temp += '2.jpg';
+//      }
+//      $(this).attr('src', temp);
+//  })
+//
+//})(jQuery); // End of use strict
